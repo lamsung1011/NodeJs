b1aae708294fcf553c99f730c94fe841be801621
lamsung1011 <nguyen.siege@gmail.com>
2023-04-19 01:27:31 +0700

    test git

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('run request ...')
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
+    res.write('<h2>Hello world! </h2>');
+    res.write('<h3>This is Mù </h3>');
     res.end();
 })
 
